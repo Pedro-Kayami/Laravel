1096d89cc03f7bcae440780f5255ebb981921611
Pedro Kayami <pedro.k.franchini@gmail.com>
2023-05-02 20:56:40 -0300

    crud completo

--- CrudLaravel/routes/web.php
+++ CrudLaravel/routes/web.php
@@ -22,21 +22,21 @@ Route::get(
 );
 Route::get(
     '/admin/cursos/adicionar',
-    ['as' => 'admin.cursos.adicionar', 'uses' => 'App\Htpp\Controllers\Admin\CursoController@adicionar']
+    ['as' => 'admin.cursos.adicionar', 'uses' => 'App\Http\Controllers\Admin\CursoController@adicionar']
 );
 Route::post(
     '/admin/cursos/salvar',
-    ['as' => 'admin.cursos.salvar', 'uses' => 'App\Htpp\Controllers\Admin\CursoController@salvar']
+    ['as' => 'admin.cursos.salvar', 'uses' => 'App\Http\Controllers\Admin\CursoController@salvar']
 );
 Route::get(
     '/admin/cursos/editar/{id}',
-    ['as' => 'admin.cursos.editar', 'uses' => 'App\Htpp\Controllers\Admin\CursoController@editar']
+    ['as' => 'admin.cursos.editar', 'uses' => 'App\Http\Controllers\Admin\CursoController@editar']
 );
 Route::put(
     '/admin/cursos/atualizar/{id}',
-    ['as' => 'admin.cursos.atualizar', 'uses' => 'App\Htpp\Controllers\Admin\CursoController@atualizar']
+    ['as' => 'admin.cursos.atualizar', 'uses' => 'App\Http\Controllers\Admin\CursoController@atualizar']
 );
 Route::get(
     '/admin/cursos/excluir/{id}',
-    ['as' => 'admin.cursos.excluir', 'uses' => 'App\Htpp\Controllers\Admin\CursoController@excluir']
+    ['as' => 'admin.cursos.excluir', 'uses' => 'App\Http\Controllers\Admin\CursoController@excluir']
 );
